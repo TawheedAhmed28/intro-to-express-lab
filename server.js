da1c86bb293d2dcd2db80d67164a7db4361e7fd9
Tawheed Ahmed <tawheedahmed42@outlook.com>
2024-05-13 16:54:56 +0100

    completed task

--- server.js
+++ server.js
@@ -19,3 +19,28 @@ app.get("/greetings/:username", (req, res) => {
 
 // ? Task 2
 
+app.get("/roll/:number", (req, res) => {
+    let number = Number(req.params.number)
+    let randomIndex = Math.floor(Math.random() * number)
+    if (!number) {
+        res.send(`How am I supposed to choose a number between 1 and ${req.params.number}?`)
+    } else {
+        res.send(`You rolled a ${randomIndex}.`)
+    }
+})
+
+// ? Task 3
+
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+  app.get("/collectibles/:index", (req, res) => {
+    if (!collectibles[req.params.index]) {
+        res.send(`This item is not yet in stock. Check back soon!`)
+    } else {
+        res.send(`So, you want the ${collectibles[req.params.index].name}? For ${collectibles[req.params.index].price}, it can be yours!`)
+    }
+  })
